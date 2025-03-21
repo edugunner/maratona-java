826d3defaaf6459b818fcf38df66f11cccda0383
edugunner <edu.santana.gunner@gmail.com>
2025-03-20 23:11:46 -0300

    feature: Exercicios: Criar uma calculadora para numeros primos e Criar um array de numeros e multiplicar cada numero dentro dele por 2

--- src/Bintroducaometodos/dominio/Calculadora.java
+++ src/Bintroducaometodos/dominio/Calculadora.java
@@ -25,20 +25,21 @@ public class Calculadora {
             System.out.println("Não Existe Divisão por Zero ");
             return;
         }
-            System.out.println(num1 / num2);
+        System.out.println(num1 / num2);
 
     }
+
     public void alteraDoisNumeros(int num1, int num2) {
         num1 = 99;
         num2 = 33;
         System.out.println("Dentro do altera dois numeros ");
-        System.out.println("num1 "+num1);
-        System.out.println("num2 "+num2);
+        System.out.println("num1 " + num1);
+        System.out.println("num2 " + num2);
     }
 
     public void somaArray(int[] numeros) {
         int soma = 0;
-        for (int num: numeros) {
+        for (int num : numeros) {
             soma += num;
         }
         System.out.println(soma);
@@ -46,10 +47,38 @@ public class Calculadora {
 
     public void VarArgs(int... numeros) {
         int soma = 0;
-        for (int num: numeros) {
+        for (int num : numeros) {
             soma += num;
         }
         System.out.println(soma);
     }
+
+    public boolean numPrimo(int num) {
+
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+    public int listaVezesDois(int... lista) {
+        int multiplicacao = 2;
+        int result = 2;
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println(lista[i] * multiplicacao);
+
+        }
+
+        return 0;
+    }
 }
+
+
 
---
+++ src/Bintroducaometodos/test/CalculadoraTest07.java
@@ -0,0 +1,12 @@
+package Bintroducaometodos.test;
+
+import Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest07 {
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        int[] Lista = {2, 4, 7,8,5,10};
+        calculadora.listaVezesDois(Lista);
+    }
+}
