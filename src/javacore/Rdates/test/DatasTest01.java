c843ad4248074a3a497dc6c436d248646a0a9db3
edugunner <edu.santana.gunner@gmail.com>
2025-06-11 23:27:04 -0300

    feature: Classes Date e Calendar

---
+++ src/javacore/Rdates/test/DatasTest01.java
@@ -0,0 +1,12 @@
+package javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DatasTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1749694564015l);// long 100000
+        System.out.println(date);
+
+        //Long, desde 1970
+    }
+}
